8a7455f24bf32e20114c82f726b398d64532fe75
Thiago Nobre <thiago.nobre2@gmail.com>
2015-04-02 00:04:23 -0300

    adicionando logs no Application_Error

--- Source/CSharp/IOC.Web/Global.asax.cs
+++ Source/CSharp/IOC.Web/Global.asax.cs
@@ -12,12 +12,15 @@ using IOC.Business.Implementation;
 using System.Reflection;
 using IOC.FW.Core.Factory;
 using IOC.FW.Web.MVC.SimpleInjector;
+using IOC.FW.Core.Logging;
 
 namespace IOC.Web
 {
     public class MvcApplication 
         : System.Web.HttpApplication
     {
+        private log4net.ILog _log;
+
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
@@ -47,6 +50,26 @@ namespace IOC.Web
                     // se ocorrer uma exception neste local, significa que você 
                     // está rodando no VS 2010 WebServer, em um ambiente de IIS/IIS Express não ocorrerá este problema
                 }
+            }
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            if (_log == null)
+            {
+                _log = LogFactory.CreateLog();
+            }
+
+            var ex = Server.GetLastError();
+
+            if (!(ex is HttpException) || ((HttpException)ex).GetHttpCode() != 404)
+            {
+                _log.Error(String.Format(
+                        "Erro na seguinte url => {0} {1}",
+                        Context.Request.HttpMethod,
+                        Context.Request.Url.AbsoluteUri
+                    ), ex);
+
             }
         }
     }
