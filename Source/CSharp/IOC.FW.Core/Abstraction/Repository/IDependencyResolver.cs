6c13672b20a12b0b1385d9747c0efecd675fce09
Thiago Carreño <thiago_carreno@outlook.com>
2015-12-02 16:53:34 -0200

    Remove all files.

--- Source/CSharp/IOC.FW.Core/Abstraction/Repository/IDependencyResolver.cs
+++
@@ -1,9 +0,0 @@
-﻿using IOC.FW.Core.Abstraction.Container;
-
-namespace IOC.FW.Core.Abstraction.Repository
-{
-    public interface IDependencyResolver
-    {
-        void Resolve(IAdapter adapter);
-    }
-}
--- Source/CSharp/IOC.Web/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IOC.Web.MvcApplication" Language="C#" %>
