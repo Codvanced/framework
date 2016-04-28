8bd86b20b2561ad5c6a93a5b42c9e272b633be46
Thiago Roberto Carreño <thiago.carreno@outlook.com>
2016-04-27 23:13:10 -0300

    Move EntityFramework classes to new assembly "IOC.FW.Repository.EF6".

---
+++ source/CSharp/IoCFramework/IOC.FW.Repository.EF6/EntityFrameworkContextFactory.cs
@@ -0,0 +1,30 @@
+﻿using System.Data.Common;
+using IOC.FW.Abstraction.Repository;
+
+namespace IOC.FW.Repository.EF6
+{
+    public class EntityFrameworkContextFactory<TModel>
+        : IContextFactory<TModel>
+        where TModel : class, new()
+    {
+        public IContext<TModel> GetContext()
+        {
+            return new EntityFrameworkContext<TModel>();
+        }
+
+        public IContext<TModel> GetContext(
+            string nameOrConnectionString
+        )
+        {
+            return new EntityFrameworkContext<TModel>(nameOrConnectionString);
+        }
+
+        public IContext<TModel> GetContext(
+            DbConnection connection,
+            bool destroyAfterUse
+        )
+        {
+            return new EntityFrameworkContext<TModel>(connection, destroyAfterUse);
+        }
+    }
+}
