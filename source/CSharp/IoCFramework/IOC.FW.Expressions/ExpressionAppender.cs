3c44f1211e7ed6ab2be14ccbc91cc26a0f2169cc
Thiago Roberto Carreño <thiago.carreno@outlook.com>
2016-09-04 14:43:47 -0300

    Move class "ExpressionAppender" to new assembly.
    #87

--- source/CSharp/IoCFramework/IOC.FW.Expressions/ExpressionAppender.cs
+++ source/CSharp/IoCFramework/IOC.FW.Expressions/ExpressionAppender.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Linq.Expressions;
 
-namespace IOC.FW.Shared.Model.Repository
+namespace IOC.FW.Expressions
 {
     public class ExpressionAppender<TModel>
     {
